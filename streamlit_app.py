d986ed0255af872c9d194a7fbe3e650477d4ac33
Alina Yildir <ms.alina.yildir@gmail.com>
2025-07-20 19:26:38 -0600

    app

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,10 +105,10 @@ def show_literature_review():
     **X(τ,f) = ∫ from -∞ to ∞ of x(t)g(t-τ) * exp(-j2πft) dt**
 
     Where:
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**x(t)**: Analyzed signal (EEG waveform)
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**g(t−τ)**: Windowing function (Hann window in their implementation)
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**τ**: Time-shifting parameter
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**f**: Frequency parameter
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**x(t)**: Analyzed signal (EEG waveform)  
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**g(t−τ)**: Windowing function (Hann window in their implementation)  
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**τ**: Time-shifting parameter  
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**f**: Frequency parameter  
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**exp(-j2πft)**: Complex exponential function used in the Fourier transform
 
     This integral allows the signal to be analyzed over a given time interval (τ) and at a given frequency (f), enabling simultaneous analysis of both time and frequency information. When g(t−τ) is considered a windowing function, the STFT analyzes both the time and frequency information of a signal simultaneously, providing the foundation for spectrogram generation.
